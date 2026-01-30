3d2a7895618c276c0e567c50818f8e0c1711ff88
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2026-01-30 22:48:53 +0200

    Update hp_system.gd

--- 2026-gamejam/common/scripts/hp_system.gd
+++ 2026-gamejam/common/scripts/hp_system.gd
@@ -5,7 +5,15 @@ var max_hp
 var current_hp
 func _ready() -> void:
 	current_hp=max_hp
+	
+	
 func take_damage(dmg:float):
 	damage_taken.emit(dmg)
+	if(current_hp-dmg>0):
+		current_hp-=dmg
+	else:
+		die()
+		
+		
 func die():
 	on_death.emit()
