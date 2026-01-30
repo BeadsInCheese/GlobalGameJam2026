12c572c1437b31c8898949b5759168b3044b731a
BeadsInCheese <hirvittavanhauskaa@gmail.com>
2026-01-31 00:12:48 +0200

    ammo counter bouncy bullets

--- 2026-gamejam/bullet/scripts/bullet.gd
+++ 2026-gamejam/bullet/scripts/bullet.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 var direction:Vector2
-var speed=40
+var speed=10
 var damage=1
+var bounces=2
 
 @export var modifiers:Array[BulletModifier]
 func _ready() -> void:
@@ -14,15 +15,20 @@ func _process(delta: float) -> void:
 	rotation=-atan2(direction.x,direction.y)
 	var collision_info = move_and_collide(direction*speed)
 	if(collision_info):
-		on_collision(collision_info.get_collider())
-
+		on_collision(collision_info.get_collider(),collision_info.get_normal())
+	
 func _on_kill_timer_timeout() -> void:
 	queue_free()
 
 
-func on_collision(body) -> void:
+func on_collision(body,normal) -> void:
 	for modifier in modifiers:
 		modifier.on_collision(self,body)
 	if body.has_method("take_damage"):
 		body.take_damage(damage)
-	queue_free()
+	if bounces<=0:
+		queue_free()
+	else:
+		direction=-direction.reflect(normal)
+		position+=direction*5
+		bounces-=1
