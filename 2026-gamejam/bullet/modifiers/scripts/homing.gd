54633f7f8efbb5a88941891dbf96d50e4e1a8682
Eetu Rantala <rantala.eetu@gmail.com>
2026-01-31 19:54:00 +0200

    Homing bullets won't target player anymore

--- 2026-gamejam/bullet/modifiers/scripts/homing.gd
+++ 2026-gamejam/bullet/modifiers/scripts/homing.gd
@@ -4,7 +4,7 @@ class_name Homing
 
 func on_process(bullet):
 	for object in bullet.get_close_bodies():
-		if object.has_method("take_damage"):
+		if object.has_method("take_damage") and object is not Player:
 			bullet.direction += (object.global_position - bullet.global_position) * 0.01
 			break
 	bullet.direction = bullet.direction.normalized()
