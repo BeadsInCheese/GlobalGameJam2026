3f66f055ec34f76045c28a05df5152bcf8f4124b
Eetu Rantala <rantala.eetu@gmail.com>
2026-01-31 12:57:34 +0200

    Add navigation for enemies

--- 2026-gamejam/enemy/scripts/enemy.gd
+++ 2026-gamejam/enemy/scripts/enemy.gd
@@ -6,12 +6,11 @@ var player
 func _ready() -> void:
 	player = find_parent("game").get_node("Player")
 
-func _process(delta):
-	var velocity = Vector2()
-	
-	velocity.x += player.position.x - position.x
-	velocity.y += player.position.y - position.y
-	move_and_collide(velocity.normalized())
+func _physics_process(delta: float) -> void:
+	$NavigationAgent2D.target_position = player.position
+	var next_path = $NavigationAgent2D.get_next_path_position()
+	var motion = next_path - position
+	move_and_collide(motion.normalized())
 
 func take_damage(f: float):
 	get_node("HPSystem").take_damage(f)
